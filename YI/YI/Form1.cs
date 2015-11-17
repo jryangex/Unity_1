b4541279c91002aa01e1cc265652f6372e70f7ed
智勝 <a7834118andy@yahoo.com.tw>
2015-11-17 16:59:36 +0800

    測試註解

--- YI/YI/Form1.cs
+++ YI/YI/Form1.cs
@@ -16,5 +16,10 @@ namespace YI
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //465456465456465456
+        }
     }
 }
